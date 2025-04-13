a2300392b68f5069241acd68957f4990339156a5
ayush-kumar-prog <ayushkumargithub@gmail.com>
2025-04-13 13:11:02 +0100

    Initial Commit

---
+++ WebsiteListApp/WebsiteListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsiteListAppApp.swift
+//  WebsiteListApp
+//
+//  Created by Ayush Kumar on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WebsiteListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
